fd6c49b12fb2e45c198f3ed977bc5eb2b997eee5
conradmazing <franciscoc1410@gmail.com>
2018-10-14 11:15:24 +0800

    updated user management and auth for child device v1

--- app/src/main/java/uic/com/imperiumchild/Login.java
+++ app/src/main/java/uic/com/imperiumchild/Login.java
@@ -24,7 +24,6 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.support.design.widget.Snackbar;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
@@ -37,12 +36,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 /**
  * Created by Conrad Francisco Jr on 6/12/2018.
@@ -198,126 +191,128 @@ public class Login extends AppCompatActivity {
 
     public void logins(final String usernem){
 
-        final String ape[]  = user.split("@");
-        if(usernem.equals(ape[0])){
+        try{
 
-            System.out.println("Current Username: "+usernem+"Current User: "+ape[0]);
-            AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
-            builder.setTitle("Please confirm action!");
-            builder.setMessage("Are all the information provided True and Correct?");
-            builder.setIcon(R.drawable.icon);
-            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            final String ape[]  = user.split("@");
+            if(usernem.equals(ape[0])){
 
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
+                System.out.println("Current Username: "+usernem+"Current User: "+ape[0]);
+                AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
+                builder.setTitle("Please confirm action!");
+                builder.setMessage("Are all the information provided True and Correct?");
+                builder.setIcon(R.drawable.icon);
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
 
-                    auth.createUserWithEmailAndPassword(user, password)
-                            .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    bar.setVisibility(View.GONE);
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
 
-                                    if(!task.isSuccessful()){
+                        auth.createUserWithEmailAndPassword(user, password)
+                                .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        bar.setVisibility(View.GONE);
 
-                                        Log.w("Registration", "signInWithCredential", task.getException());
+                                        if(!task.isSuccessful()){
 
-                                        if(task.getException() instanceof FirebaseAuthUserCollisionException) {
+                                            Log.w("Registration", "signInWithCredential", task.getException());
 
-                                            final Snackbar sn = Snackbar.make(constraint, "Account already Registered, Signing In!", Snackbar.LENGTH_INDEFINITE);
+                                            if(task.getException() instanceof FirebaseAuthUserCollisionException) {
+
+                                                final Snackbar sn = Snackbar.make(constraint, "Account already Registered, Signing In!", Snackbar.LENGTH_INDEFINITE);
+                                                sn.show();
+                                                inputuser.setText(null);
+
+                                                final String newpass = password;
+                                                final String newuser = user;
+
+                                                try {
+
+                                                    auth.signInWithEmailAndPassword(newuser, newpass)
+                                                            .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                                                                @Override
+                                                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                                                    bar.setVisibility(View.GONE);
+                                                                    sn.dismiss();
+                                                                    if(task.isSuccessful()){
+
+                                                                        Snackbar sn = Snackbar.make(constraint, "Login Successfully!", Snackbar.LENGTH_SHORT);
+                                                                        sn.show();
+                                                                        startActivity(new Intent(Login.this, MainClass.class));
+                                                                        finish();
+
+                                                                    }
+                                                                    else {
+
+                                                                        Snackbar sn = Snackbar.make(constraint, R.string.auth_failed, Snackbar.LENGTH_SHORT);
+                                                                        sn.show();
+                                                                        inputuser.setText("");
+
+                                                                    }
+
+                                                                }
+
+                                                            });
+
+                                                }
+
+                                                catch (Exception e) {
+
+                                                    Log.e("SignIn", e.getMessage(), e);
+                                                }
+
+                                            }
+
+                                            else if(task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+
+                                                Toast.makeText(Login.this, "Please check your EMail Address!", Toast.LENGTH_LONG).show();
+                                                inputuser.setText(null);
+
+                                            }
+
+                                        }
+                                        else if(task.isSuccessful()) {
+
+                                            Snackbar sn = Snackbar.make(constraint, "Login Successfully!", Snackbar.LENGTH_SHORT);
                                             sn.show();
-                                            inputuser.setText(null);
+                                            startActivity(new Intent(Login.this, MainClass.class));
+                                            finish();
 
-                                            final String newpass = password;
-                                            final String newuser = user;
-
-                                            try {
-
-                                                auth.signInWithEmailAndPassword(newuser, newpass)
-                                                        .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
-                                                            @Override
-                                                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                                                bar.setVisibility(View.GONE);
-                                                                sn.dismiss();
-                                                                if(task.isSuccessful()){
-
-                                                                    Snackbar sn = Snackbar.make(constraint, "Login Successfully!", Snackbar.LENGTH_SHORT);
-                                                                    sn.show();
-                                                                    startActivity(new Intent(Login.this, MainClass.class));
-                                                                    finish();
-
-                                                                }
-                                                                else {
-
-                                                                    Snackbar sn = Snackbar.make(constraint, R.string.auth_failed, Snackbar.LENGTH_SHORT);
-                                                                    sn.show();
-                                                                    inputuser.setText("");
-
-                                                                }
-
-                                                            }
-
-                                                        });
-
-                                            }
-
-                                            catch (Exception e) {
-
-                                                Log.e("SignIn", e.getMessage(), e);
-                                            }
 
                                         }
-
-                                        else if(task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
-
-                                            Toast.makeText(Login.this, "Please check your EMail Address!", Toast.LENGTH_LONG).show();
-                                            inputuser.setText(null);
-
-                                        }
-
                                     }
-                                    else if(task.isSuccessful()) {
+                                });
+                    }
+                });
 
-                                        Snackbar sn = Snackbar.make(constraint, "Login Successfully!", Snackbar.LENGTH_SHORT);
-                                        sn.show();
-                                        startActivity(new Intent(Login.this, MainClass.class));
-                                        finish();
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
 
+                        dialogInterface.dismiss();
+                        Toast.makeText(getApplicationContext(), "Log-In Cancelled!", Toast.LENGTH_SHORT).show();
+                        inputuser.setText(null);
+                        bar.setVisibility(View.GONE);
 
-                                    }
-                                }
-                            });
-                }
-            });
+                    }
+                });
+                android.support.v7.app.AlertDialog alert = builder.create();
+                alert.show();
 
-            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
+            }
 
-                    dialogInterface.dismiss();
-                    Toast.makeText(getApplicationContext(), "Log-In Cancelled!", Toast.LENGTH_SHORT).show();
-                    inputuser.setText(null);
-                    bar.setVisibility(View.GONE);
+            else {
 
-                }
-            });
-            android.support.v7.app.AlertDialog alert = builder.create();
-            alert.show();
+                Log.d("Sign In", "Sign-in Failed");
+
+            }
 
         }
-//
-//        else if(!usernem.equals(ape[0])) {
-//
-//            Snackbar sn = Snackbar.make(constraint, "2 This Account has not been added by a Parent Device!", Snackbar.LENGTH_LONG);
-//            sn.show();
-//            inputuser.setText(null);
-//
-//        }
 
-        else {
+        catch(Exception e){
 
-            Log.d("Sign In", "Sign-in Failed");
-
+            Log.e("on Sign In", e.getMessage(), e);
         }
+
 
     }
     @Override
